03f303038a8ce0e69c21d6d2e334fe1e29752bf3
George <xsova113@gmail.com>
2023-07-16 13:24:35 -0700

    minor update

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -10,16 +10,14 @@ const Contact = () => {
       <Container className="pt-16">
         <SectionHeader number={3} title={"contact"} />
         <div className="w-full flex flex-col space-y-6 mt-10 text-center">
-          <h1 className="font-bold text-4xl">Get In Touch</h1>
-          <span className="text-muted-foreground w-2/3 self-center pb-8">
-            Amet ullamco laborum officia deserunt mollit esse anim occaecat.
-            Tempor occaecat in elit laboris dolore dolor culpa incididunt
-            eiusmod nisi elit ut.
-          </span>
+          <h1 className="font-bold sm:text-5xl text-3xl text-slate-300 pb-4">Get In Touch</h1>
+          <h3 className="text-xl text-yellow-500">Open for freelance work or collaboration</h3>
+          <p className="text-muted-foreground sm:w-1/2 self-center pb-8">
+            If you need to build a professional looking website and application,
+            or just want to say hi, feel free to contact me!
+          </p>
           <Button className="w-[160px] self-center bg-yellow-500 hover:bg-yellow-300 transition">
-            <a href="mailto:georgechangh@gmail.com">
-              Send A Message
-            </a>
+            <a href="mailto:georgechangh@gmail.com">Send A Message</a>
           </Button>
         </div>
       </Container>
