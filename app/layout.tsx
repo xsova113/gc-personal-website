555b8882fcad2afd5b65067b2422e90d3c79c189
George <xsova113@gmail.com>
2023-11-15 18:23:41 -0800

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import { ThemeProvider } from "@/providers/theme-provider";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
@@ -22,11 +23,14 @@ export default function RootLayout({
       <head>
         <link rel="icon" type="image/svg+xml" href="/images/logo-gold.png" />
       </head>
-      <body className={`${robotoMono.className} max-w-screen-xl h-full dark:bg-black min-w-full`}>
+      <body
+        className={`${robotoMono.className} max-w-screen-xl h-full dark:bg-black min-w-full`}
+      >
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <NavBar />
           {children}
           <Footer />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
